3ebc64331ce799c3ad92ff15eda8fac4d2fff588
hxviet <60069200+hxviet@users.noreply.github.com>
2023-11-27 17:44:22 +0700

    display docker rm and docker run messages

--- start-container.sh
+++ start-container.sh
@@ -4,29 +4,36 @@
 N=${1:-3}
 
 
-# start hadoop master container
-sudo docker rm -f hadoop-master &> /dev/null
-echo "start hadoop-master container..."
+# start new hadoop master container
+if [ "$(sudo docker ps -aq -f name=hadoop-master)" ]; then
+	echo "Removing existing container 'hadoop-master'"
+	sudo docker rm -f hadoop-master
+fi
+echo "Starting hadoop-master container..."
 sudo docker run -itd \
                 --net=hadoop \
                 -p 50070:50070 \
                 -p 8088:8088 \
                 --name hadoop-master \
                 --hostname hadoop-master \
-                kiwenlau/hadoop:1.0 &> /dev/null
+                kiwenlau/hadoop:1.0
 
 
-# start hadoop slave container
+# start new hadoop slave containers
 i=1
 while [ $i -lt $N ]
 do
-	sudo docker rm -f hadoop-slave$i &> /dev/null
-	echo "start hadoop-slave$i container..."
+	slave_container="hadoop-slave$i"
+	if [ "$(sudo docker ps -aq -f name=$slave_container)" ]; then
+		echo "Removing existing container '$slave_container'"
+		sudo docker rm -f $slave_container
+	fi
+	echo "Starting hadoop-slave$i container..."
 	sudo docker run -itd \
 	                --net=hadoop \
-	                --name hadoop-slave$i \
-	                --hostname hadoop-slave$i \
-	                kiwenlau/hadoop:1.0 &> /dev/null
+	                --name $slave_container \
+	                --hostname $slave_container \
+	                kiwenlau/hadoop:1.0
 	i=$(( $i + 1 ))
 done 
 
